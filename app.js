e0ad2183d3dcd16d65b0fa3be25cec99cb4267da
Deorel <Deorel251098@gmail.com>
2023-02-09 22:18:44 +0700

    add: parsing fipi and activator

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mysqlAdmin from 'node-mysql-admin';
 import startBrowser  from './src/modules/parser/startBrowser.js';
 import scraperController from './src/controllers/pageController.js';
+import fipiScraper from './src/controllers/fipiBrowserController.js';
 import { api } from './src/routes/router.js';
 import cors from 'cors'
 
@@ -17,12 +18,28 @@ app.use('/api', api)
 app.use('/img', express.static('./src/data/img/emblems/'))
 
 
+const UpdateFipiInfo = () => {
+   let browserInstance = startBrowser.startBrowser();
+   fipiScraper(browserInstance)
+}
 
 const ParsingActivate = () => {
    let browserInstance = startBrowser.startBrowser();
    scraperController(browserInstance)
+
+   let nowDate = new Date()
+   let needDate = new Date(`${nowDate.getFullYear()}-${nowDate.getMonth() + 1}-2`)
+   nowDate = nowDate.toISOString().split('T')[0]
+   needDate = needDate.toISOString().split('T')[0]
+   if (nowDate === needDate){
+      UpdateFipiInfo()
+   }
 }
 
+
+
+
+// UpdateFipiInfo()
 // ParsingActivate()
 
 
---
+++ src/modules/parser/fipi/index.js
@@ -0,0 +1,2 @@
+export { default as ege } from './ege.js';
+export { default as oge } from './oge.js';
